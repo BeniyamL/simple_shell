450b737cdb0efdd97ee1760d7c4a3d7a336951c7
Bereket Abera <bereket5042@gmail.com>
2021-04-01 22:36:37 +0000

    Add initial manual page

---
+++ get_command_path.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/**
+ * get_command_path - get the executable path of a command
+ * @command: a command to find
+ *
+ * Return: the absolute path of the command
+ */
+char *get_command_path(char *command)
+{
+
+}
---
+++ holberton.h
@@ -0,0 +1,5 @@
+#ifndef DEFINE_PROTO
+#define DEFINE_PROTO
+char *_getline(char *prName);
+char *get_command_path(char *command);
+#endif
